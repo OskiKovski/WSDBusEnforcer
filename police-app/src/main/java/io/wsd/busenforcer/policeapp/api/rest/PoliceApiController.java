1a6eaffddc59dd8dd792e07fbd1d15ac0cfdb51f
jkumor <elohhim@gmail.com>
2018-12-09 21:18:59 +0100

    Cleanup

--- police-app/src/main/java/io/wsd/busenforcer/policeapp/api/rest/PoliceApiController.java
+++ police-app/src/main/java/io/wsd/busenforcer/policeapp/api/rest/PoliceApiController.java
@@ -2,10 +2,12 @@ package io.wsd.busenforcer.policeapp.api.rest;
 
 import io.wsd.busenforcer.agents.common.model.Location;
 import io.wsd.busenforcer.policeapp.client.dto.SummaryDTO;
-import io.wsd.busenforcer.policeapp.service.AgentService;
 import io.wsd.busenforcer.policeapp.service.RoutingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/police/api")
--- police-app/src/main/java/io/wsd/busenforcer/policeapp/client/configuration/ApiORSConfiguration.java
+++ police-app/src/main/java/io/wsd/busenforcer/policeapp/client/configuration/ApiORSConfiguration.java
@@ -1,13 +1,13 @@
 package io.wsd.busenforcer.policeapp.client.configuration;
 
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import feign.FeignException;
 import feign.Response;
 import feign.codec.DecodeException;
 import feign.codec.Decoder;
 import io.wsd.busenforcer.policeapp.client.dto.SummaryDTO;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 
 import java.io.IOException;
